fc6dbbfb1f49a705ff3f0c4f0bdcffa768747e9a
dacamposk <daniel_chat@live.com>
2026-09-11 04:39:53 -0300

    Archivos media a prodducion

--- musicpro/urls.py
+++ musicpro/urls.py
@@ -18,6 +18,8 @@ from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings 
 from django.conf.urls.static import static
+from django.views.static import serve
+from django.urls import re_path
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -25,5 +27,7 @@ urlpatterns = [
     path('accounts/', include('django.contrib.auth.urls')),
     path('cart/', include('carts.urls')),
     path('orders/', include('orders.urls')),
+    re_path(r'^media/(?P<path>.*)$', serve, {'document_root': settings.MEDIA_ROOT}),
+
 
 ]  + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
